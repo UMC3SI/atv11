7a6bbce138395b1c61f7b47d1c253da9b9be5b5f
Allan475 <100589208+Allan475@users.noreply.github.com>
2022-05-30 21:53:49 -0300

    alteração

---
+++ src/model/Aluno.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class Aluno {
+    
+    private String nome;
+    private String snome;
+    private String data;
+    private String sexo;
+    private String RGM;
+    private String curso;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSnome() {
+        return snome;
+    }
+
+    public void setSnome(String snome) {
+        this.snome = snome;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getRGM() {
+        return RGM;
+    }
+
+    public void setRGM(String RGM) {
+        this.RGM = RGM;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    
+    
+    
+}
